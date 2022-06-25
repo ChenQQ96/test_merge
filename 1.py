5d5a649b0b6f9c97c6e7a00a1ad53eb1c8636040
chenqiangqiang.cqq <chenqiangqiang.cqq@alibaba-inc.com>
2022-06-26 00:47:16 +0800

    1

--- 1.py
+++ 1.py
@@ -1,8 +1,11 @@
 #本地分支创建的文件
-
 git branch —— 查看本地分支
 git branch $开发分支名 —— 创建开发分支
 git checkout $分支名 ——切换到分支名
 git add
 git commit
 git push origin $开发分支 —— 提交到远程开发分支
+
+#merge
+git checkout $主干分支 —— 切换到主干分支
+git merge $开发分支 —— #合开发分支到master
