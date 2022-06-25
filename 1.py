ee4c405724ebff174e54120268fe3dbfd01206f0
chenqiangqiang.cqq <chenqiangqiang.cqq@alibaba-inc.com>
2022-06-26 01:37:01 +0800

    3

--- 1.py
+++ 1.py
@@ -6,17 +6,19 @@ git add
 git commit
 git push origin $开发分支 —— 提交到远程开发分支
 
-#merge
+
 git checkout $主干分支 —— 切换到主干分支
 git merge $开发分支 —— 合并开发分支到本地主干分支
     如果发生代码冲突conflict：git merge --abort —— 回滚merge操作
 git push —— 提交大远程主干分支
+#merge
 
 #pull
-方法—：
-git fetch origin master  —— 获取远程分支到本地
-git log -p master..origin/master —— 查看远程master分支与本地master分支的差别
 git merge origin/master
 方法二：
 git pull origin $主干分支 ——— 拉取主干分支，合并到开发分支
+方法—：
+git fetch origin master  —— 获取远程分支到本地
+git log -p master..origin/master —— 查看远程master分支与本地master分支的差别
+
 
